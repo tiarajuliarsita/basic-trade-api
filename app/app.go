0fa9f9774a09ee7689eef1e8e221e9f5c61baf5f
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-09-18 00:58:21 +0800

    add port

--- app/app.go
+++ app/app.go
@@ -3,8 +3,11 @@ package app
 import (
 	"final-project/database"
 	"final-project/routes"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func App() {
@@ -12,6 +15,12 @@ func App() {
 	database.ConnectDB()
 
 	routes.Routes(app)
-	app.Run(":8080")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	app.Run(port)
 
 }
