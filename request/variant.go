a2af4baf04cbed489aaa574307cdf4d8b7122d15
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-10-01 23:52:27 +0800

    modify request variants

--- request/variant.go
+++ request/variant.go
@@ -3,5 +3,5 @@ package request
 type Variant struct {
 	VariantName string `json:"variant_name" form:"variant_name" valid:"required~variant~name is required"`
 	Quantity    int    `json:"quantity" form:"quantity" valid:"required~entity is required"`
-	UUID        string   `json:"uuid" form:"uuid"`
+	UUID        string   `json:"product_id" form:"product_id"`
 }
